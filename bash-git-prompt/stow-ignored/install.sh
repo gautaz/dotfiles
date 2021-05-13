a46e092befc552f56ff75d6edce84c002658e0c0
gautaz <gautaz.github@gmail.com>
2021-05-14 00:19:27 +0200

    replace bash-git-prompt by pureline

--- bash-git-prompt/stow-ignored/install.sh
+++
@@ -1,2 +0,0 @@
-rm -rf "${HOME}/.bash-git-prompt"
-git clone https://github.com/magicmonty/bash-git-prompt.git "${HOME}/.bash-git-prompt" --depth=1
---
+++ pureline/stow-ignored/install.sh
@@ -0,0 +1,2 @@
+rm -rf "${HOME}/.pureline"
+git clone --depth=1 git@github.com:chris-marsh/pureline.git "${HOME}/.pureline"
