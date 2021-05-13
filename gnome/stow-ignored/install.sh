e9b0971e52880cdfa377fd3c372b01307d1d788d
gautaz <gautaz.github@gmail.com>
2021-05-13 14:18:37 +0200

    gnome keybindings

---
+++ gnome/stow-ignored/install.sh
@@ -0,0 +1,16 @@
+if hash -r dconf 2> /dev/null; then
+dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ <<- END_OF_CONFIGURATION
+[custom0]
+binding='<Alt>q'
+command='tilix --quake'
+name='Tilix Quake'
+
+[custom1]
+binding='<Alt>s'
+command='flameshot gui'
+name='Flameshot'
+END_OF_CONFIGURATION
+__highlight
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ | awk -F "[=']+" '/^binding=/{BINDING=$2}/^name=/{print BINDING " bound to " $2}'
+__default
+fi
