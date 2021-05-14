2e7198225a7c8beca318588d9d5c78fc0bbbc284
gautaz <gautaz.github@gmail.com>
2021-05-14 04:01:46 +0200

    auto install git-porcelain

---
+++ git-porcelain/stow-ignored/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+GIT_PORCELAIN_HOME="$(dirname "${PWD}")/git-porcelain"
+if [ ! -d "${GIT_PORCELAIN_HOME}" ]; then
+	git clone git@github.com:gautaz/git-porcelain.git "${GIT_PORCELAIN_HOME}"
+else
+	(cd "${GIT_PORCELAIN_HOME}" && git fetch -a -p && git pull)
+fi
+rm -f "${HOME}/.git-porcelain"
+ln -s "${GIT_PORCELAIN_HOME#${HOME}/}" "${HOME}/.git-porcelain"
