3ca6d7b9050aed5118cad865d6bff780ff9e1235
gautaz <gautaz.github@gmail.com>
2021-05-26 09:43:25 +0200

    npm & neovim-node-host node modules per default

--- volta/stow-ignored/install.sh
+++ volta/stow-ignored/install.sh
@@ -1,3 +1,7 @@
 curl -s https://get.volta.sh | bash -s -- --skip-setup
+
 "${HOME}/.volta/bin/volta" install node
+"${HOME}/.volta/bin/volta" install npm
+
+"${HOME}/.volta/bin/volta" install neovim-node-host
 "${HOME}/.volta/bin/volta" install open-cli
