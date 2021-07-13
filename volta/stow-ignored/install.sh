b2379307c437ac9823332a440d72b066c5beaa7f
gautaz <gautaz.github@gmail.com>
2021-07-13 14:30:43 +0200

    fix neovim module since renaming

--- volta/stow-ignored/install.sh
+++ volta/stow-ignored/install.sh
@@ -3,5 +3,5 @@ curl -s https://get.volta.sh | bash -s -- --skip-setup
 "${HOME}/.volta/bin/volta" install node
 "${HOME}/.volta/bin/volta" install npm
 
-"${HOME}/.volta/bin/volta" install neovim-node-host
+"${HOME}/.volta/bin/volta" install neovim
 "${HOME}/.volta/bin/volta" install open-cli
