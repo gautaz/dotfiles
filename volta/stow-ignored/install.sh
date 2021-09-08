eed171c674552ebd586b7f69e1452035f1fd753f
gautaz <11046232+gautaz@users.noreply.github.com>
2021-09-08 14:19:08 +0200

    faster bashrc: write volta/done/npm completion

--- volta/stow-ignored/install.sh
+++ volta/stow-ignored/install.sh
@@ -5,3 +5,9 @@ curl -s https://get.volta.sh | bash -s -- --skip-setup
 
 "${HOME}/.volta/bin/volta" install neovim
 "${HOME}/.volta/bin/volta" install open-cli
+
+VOLTA_COMPLETION="volta/.bash_completion.d"
+mkdir -p "${VOLTA_COMPLETION}"
+volta completions bash > "${VOLTA_COMPLETION}/volta"
+node --completion-bash > "${VOLTA_COMPLETION}/node"
+npm completion > "${VOLTA_COMPLETION}/npm"
