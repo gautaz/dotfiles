88b2da61450d885c8df645109c171c79451199cf
gautaz <11046232+gautaz@users.noreply.github.com>
2022-02-05 11:17:14 +0100

    back to old style kubectl install
    using a docker image is too much hassle

--- kubectl/stow-ignored/install.sh
+++ kubectl/stow-ignored/install.sh
@@ -1,18 +1,13 @@
-#!/usr/bin/env bash
-mkdir -p "${HOME}/.kube/config.d"
-if [ ! -e "${HOME}/.kube/config" ]; then
-	cat > "${HOME}/.kube/config" <<- EOC
-	apiVersion: v1
-	clusters: null
-	contexts: null
-	kind: Config
-	preferences: {}
-	users: null
-	EOC
-fi
-. kubectl/.bashrc.d/kubectl
-if hash -r kubectl 2>/dev/null; then
-	KUBECTL_COMPLETION="kubectl/.bash_completion.d"
-	mkdir -p "${KUBECTL_COMPLETION}"
-	kubectl completion bash > "${KUBECTL_COMPLETION}/kubectl"
-fi
+KUBECTL_CMD="${HOME}/stow/kubectl/bin/kubectl"
+
+PLATFORM="$(uname -s)"
+declare -A KUBECTL_ARCH=( ["x86_64"]="amd64" ["aarch64"]="arm64" )
+
+mkdir -p "${KUBECTL_CMD%/*}"
+curl -sfL "https://dl.k8s.io/release/$(curl -sfL https://dl.k8s.io/release/stable.txt)/bin/${PLATFORM,,}/${KUBECTL_ARCH[$(uname -m)]}/kubectl" -o "${KUBECTL_CMD}"
+chmod u+x "${KUBECTL_CMD}"
+stow -d "${HOME}/stow" "kubectl"
+
+KUBECTL_COMPLETION="kubectl/.bash_completion.d"
+mkdir -p "${KUBECTL_COMPLETION}"
+"${KUBECTL_CMD}" completion bash > "${KUBECTL_COMPLETION}/kubectl"
