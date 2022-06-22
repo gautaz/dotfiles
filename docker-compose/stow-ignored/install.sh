8f77d823b622f6fdc08e01f36b79c7d8ab315c15
gautaz <11046232+gautaz@users.noreply.github.com>
2022-06-22 07:09:09 +0200

    no need for compose when docker is not available

--- docker-compose/stow-ignored/install.sh
+++ docker-compose/stow-ignored/install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-COMPOSE_CMD="${HOME}/stow/docker-compose/bin/docker-compose"
-# 1.29.2 is a workaround for https://github.com/docker/compose/issues/8550
-# COMPOSE_VERSION="$(curl -sf "https://api.github.com/repos/docker/compose/releases/latest" | awk -F'[ ":,]+' '/tag_name/{print $3}')"
-COMPOSE_VERSION=1.29.2
 
-mkdir -p "${COMPOSE_CMD%/*}"
-curl -sfL "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o "${COMPOSE_CMD}"
-chmod u+x "${COMPOSE_CMD}"
-stow -d "${HOME}/stow" "docker-compose"
+if hash -r docker 2> /dev/null; then
+	COMPOSE_CMD="${HOME}/stow/docker-compose/bin/docker-compose"
+	# 1.29.2 is a workaround for https://github.com/docker/compose/issues/8550
+	# COMPOSE_VERSION="$(curl -sf "https://api.github.com/repos/docker/compose/releases/latest" | awk -F'[ ":,]+' '/tag_name/{print $3}')"
+	COMPOSE_VERSION=1.29.2
 
-COMPOSE_COMPLETION="docker-compose/.bash_completion.d"
-mkdir -p "${COMPOSE_COMPLETION}"
-curl -sfL "https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose" -o "${COMPOSE_COMPLETION}/docker-compose"
+	mkdir -p "${COMPOSE_CMD%/*}"
+	curl -sfL "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o "${COMPOSE_CMD}"
+	chmod u+x "${COMPOSE_CMD}"
+	stow -d "${HOME}/stow" "docker-compose"
+
+	COMPOSE_COMPLETION="docker-compose/.bash_completion.d"
+	mkdir -p "${COMPOSE_COMPLETION}"
+	curl -sfL "https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose" -o "${COMPOSE_COMPLETION}/docker-compose"
+fi
