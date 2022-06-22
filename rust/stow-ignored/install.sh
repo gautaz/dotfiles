56ddcdef787b58f3b7c6998f52235f12ed58f8ec
gautaz <11046232+gautaz@users.noreply.github.com>
2022-06-22 07:10:42 +0200

    simpler way to get the needed output

--- rust/stow-ignored/install.sh
+++ rust/stow-ignored/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 stow -D -d "${HOME}/stow" "cargo"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y --no-modify-path | grep -E '^[[:alpha:]]: '
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y --no-modify-path > /dev/null
 CARGO="${HOME}/stow/cargo"
 mkdir -p "${CARGO}"
 (cd "${CARGO}" && rm -f bin && ln -s ../../.cargo/bin)
