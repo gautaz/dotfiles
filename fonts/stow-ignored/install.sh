212a98b62a9070b56471418138d6b02f15bb4af9
gautaz <11046232+gautaz@users.noreply.github.com>
2022-08-27 16:18:26 +0200

    no prompt on unzip

--- fonts/stow-ignored/install.sh
+++ fonts/stow-ignored/install.sh
@@ -9,7 +9,7 @@ mkdir -p "${FONTS}"
 (
 	cd "${FONTS}"
 	curl -sfLO "https://github.com/${NERD_FONTS_REPOSITORY}/releases/download/$(__gh_latest_tag "${NERD_FONTS_REPOSITORY}")/${NERD_FONTS_UBUNTU_ZIP}"
-	unzip -f "${NERD_FONTS_UBUNTU_ZIP}"
+	unzip -of "${NERD_FONTS_UBUNTU_ZIP}"
 	rm "${NERD_FONTS_UBUNTU_ZIP}"
 )
 
