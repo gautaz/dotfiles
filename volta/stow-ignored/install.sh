0d070a7ab57477d8a9aea216ef5ca13589fd7149
gautaz <11046232+gautaz@users.noreply.github.com>
2022-08-27 16:18:53 +0200

    yarn needed by neovim checkhealth

--- volta/stow-ignored/install.sh
+++ volta/stow-ignored/install.sh
@@ -8,6 +8,7 @@ curl -sf https://get.volta.sh | bash -s -- --skip-setup
 
 "${HOME}/.volta/bin/volta" install neovim
 "${HOME}/.volta/bin/volta" install open-cli
+"${HOME}/.volta/bin/volta" install yarn # needed by neovim checkhealth
 
 VOLTA_COMPLETION="volta/.bash_completion.d"
 mkdir -p "${VOLTA_COMPLETION}"
